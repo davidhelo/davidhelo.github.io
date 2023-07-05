02ce840929314618f2aca773a71048f5716af244
dahelo <dohernandez93@gmail.com>
2023-07-05 19:16:39 +0100

    feat: add circle background animation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import Stack from './components/Stack';
 function App() {
   return (
     <div className="App">
+    <div className="ripple-background"></div>
+    <div className="circle xxlarge shade1"></div>
+    <div className="circle xlarge shade2"></div>
+    <div className="circle large shade3"></div>
+    <div className="circle medium shade4"></div>
+    <div className="circle small shade5"></div>
       <header className="App-header">
       <img id="profile-pic"
             src="/images/pictureSQ.jpeg"
@@ -15,7 +21,7 @@ function App() {
             width="180rem"
       />
         <p>
-          Hi, I'm David Hernandez
+          Hi, I'm <span className='p-name'>David Hernandez</span>
         </p>
         <p>
           Full Stack Web Developer
@@ -23,11 +29,11 @@ function App() {
       </header>
 
       <br></br>
-      <p> Stack: </p>
-      <Stack />
-
+      <div className='p-stack'>
+        <p > Stack: </p>
+        <Stack />
+      </div>
       <br></br>
-
       <p>Projects experience:</p>
       <ProjectSection />
       
